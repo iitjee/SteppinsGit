557dc8813cbaa5a5b9446fde755f621599db8c34
iitjee <iit.jee.help.me@gmail.com>
2019-03-23 19:59:47 +0530

    Create 05GitLog.bash

---
+++ GitLyn/05GitLog.bash
@@ -0,0 +1,8 @@
+git log
+git log -n 3 //gives latest 3 commits
+git log -n 0 //no commit :p
+
+git log --since=2012-06-14
+git log --until=2012-06-14
+git log --author="IIT"
+git log --grep="Init" //regular expression
